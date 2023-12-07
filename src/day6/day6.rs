767cb472e1f55e70f0b383e307966da189133d2b
Lin <hulin2021@gmail.com>
2023-12-07 23:08:34 +0100

    day6 finish second half

--- src/day6/day6.rs
+++ src/day6/day6.rs
@@ -1,26 +1,27 @@
 use std::path::Path;
 use crate::utils::read_lines;
 
-#[derive(Debug)]
 struct RaceRecord {
-    time: u32,
-    distance: u32
+    time: u64,
+    distance: u64
 }
 
-fn beat_record(file: &Path) -> u32 {
-    let mut line_nums: Vec<Vec<u32>> = vec![];
+fn beat_record(file: &Path, combined: bool) -> u64 {
+    let mut line_nums: Vec<Vec<u64>> = vec![];
         read_lines(file)
             .unwrap()
             .for_each(|l| {
                 let strs = l.unwrap();
-                line_nums.push(strs.split(' ').into_iter().filter_map(|s| s.parse::<u32>().ok()).collect::<Vec<u32>>());
+                if combined {
+                    line_nums.push(vec![strs.split(' ').into_iter().filter_map(|s| s.parse::<u64>().ok()).map(|s| s.to_string()).collect::<Vec<String>>().join("").parse::<u64>().unwrap()]);
+                } else {
+                    line_nums.push(strs.split(' ').into_iter().filter_map(|s| s.parse::<u64>().ok()).collect::<Vec<u64>>());
+                }
             });
     let mut race_records: Vec<RaceRecord> = vec![];
     for i in 0..line_nums[0].len() {
         race_records.push(RaceRecord { time: line_nums[0][i], distance: line_nums[1][i] });
     }
-
-    println!("count: {:?}", race_records);
 
     let mut reach_count = vec![];
     for rr in race_records {
@@ -30,7 +31,6 @@ fn beat_record(file: &Path) -> u32 {
                 count += 1;
             }
         }
-        println!("count: {:?}", count);
         reach_count.push(count);
     }
 
@@ -43,25 +43,25 @@ mod day6_tests {
 
     #[test]
     fn day6_1_test() {
-        let result = beat_record(Path::new("src/day6/day6_input_test.txt"));
+        let result = beat_record(Path::new("src/day6/day6_input_test.txt"), false);
         assert_eq!(result, 288);
     }
 
     #[test]
     fn day6_1_answer() {
-        let result = beat_record(Path::new("src/day6/day6_input.txt"));
-        assert_eq!(result, 806029445);
+        let result = beat_record(Path::new("src/day6/day6_input.txt"), false);
+        assert_eq!(result, 440000);
     }
 
     #[test]
     fn day6_2_test() {
-        let result = beat_record(Path::new("src/day6/day6_input_test.txt"));
-        assert_eq!(result, 30);
+        let result = beat_record(Path::new("src/day6/day6_input_test.txt"), true);
+        assert_eq!(result, 71503);
     }
 
     #[test]
     fn day6_2_answer() {
-        let result = beat_record(Path::new("src/day6/day6_input.txt"));
-        assert_eq!(result, 6284877);
+        let result = beat_record(Path::new("src/day6/day6_input.txt"), true);
+        assert_eq!(result, 26187338);
     }
 }
