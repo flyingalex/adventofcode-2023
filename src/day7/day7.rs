5cc1635572b7237a40d35ba2f1f4837c766cda30
Lin <hulin2021@gmail.com>
2023-12-08 23:00:43 +0100

    day7 finish second half

--- src/day7/day7.rs
+++ src/day7/day7.rs
@@ -9,7 +9,7 @@ struct Hand {
     card_map: HashMap<char, u64>,
 }
 
-fn format_data(file: &Path) -> Vec<Hand> {
+fn format_data(file: &Path, has_joker: bool) -> (Vec<Hand>, HashMap<char, u64>) {
     let mut camel_cards: Vec<Hand> = vec![];
     read_lines(file)
         .unwrap()
@@ -17,6 +17,7 @@ fn format_data(file: &Path) -> Vec<Hand> {
             let strs = l.unwrap();
             let camel_card = strs.split(' ').filter(|s| !s.is_empty()).collect::<Vec<&str>>();
             let mut camel_card_map: HashMap<char, u64> = HashMap::new();
+            let mut joker_card_map: HashMap<char, u64> = HashMap::new();
 
             camel_card[0].chars().for_each(|c| {
                 if camel_card_map.get(&c).is_some() {
@@ -25,22 +26,56 @@ fn format_data(file: &Path) -> Vec<Hand> {
                     camel_card_map.insert(c, 1);
                 }
             });
-            camel_cards.push(Hand { strength: camel_card[0].parse().unwrap(), card_map: camel_card_map, bid: camel_card[1].parse::<u64>().unwrap() });
+
+            if has_joker {
+                joker_card_map = camel_card_map.clone();
+                if camel_card_map.contains_key(&'J') {
+                    joker_card_map.remove(&'J');
+                    let mut max_key = ' ';
+                    let mut max_value = u64::MIN;
+                    joker_card_map.iter().for_each(|(k, v)| {
+                        if *v > max_value {
+                            max_value = *v;
+                            max_key = *k;
+                        }
+                    });
+                    joker_card_map.insert(max_key, camel_card_map.get(&'J').unwrap() + max_value);
+                }
+                camel_card_map  = joker_card_map;
+            }
+            camel_cards.push(Hand {
+                strength: camel_card[0].parse().unwrap(),
+                card_map: camel_card_map,
+                bid: camel_card[1].parse::<u64>().unwrap()
+            });
         });
-    camel_cards
+
+    let mut j_val = 11;
+    if has_joker {
+        j_val = 1;
+    }
+    let char_rank = HashMap::from([
+        ('A', 14),
+        ('K', 13),
+        ('Q', 12),
+        ('T', 10),
+        ('9', 9),
+        ('8', 8),
+        ('7', 7),
+        ('6', 6),
+        ('5', 5),
+        ('4', 4),
+        ('3', 3),
+        ('2', 2),
+        ('J', j_val),
+    ]);
+
+    (camel_cards, char_rank)
 }
 
 // 1, 2, 2,  3, 3, 4, 5
-fn total_winnings(hands: Vec<Hand>) -> u64 {
-    let mut sort_hand_group = vec![
-        vec![],
-        vec![],
-        vec![],
-        vec![],
-        vec![],
-        vec![],
-        vec![],
-    ];
+fn total_winnings(hands: Vec<Hand>, char_rank: HashMap<char, u64>) -> u64 {
+    let mut sort_hand_group = vec![vec![], vec![], vec![], vec![], vec![], vec![], vec![]];
     for hand in hands {
         if hand.card_map.len() == 5 {
             sort_hand_group[0].push(hand);
@@ -62,21 +97,6 @@ fn total_winnings(hands: Vec<Hand>) -> u64 {
             sort_hand_group[6].push(hand);
         }
     }
-    let char_rank = HashMap::from([
-        ('A', 14),
-        ('K', 13),
-        ('Q', 12),
-        ('J', 11),
-        ('T', 10),
-        ('9', 9),
-        ('8', 8),
-        ('7', 7),
-        ('6', 6),
-        ('5', 5),
-        ('4', 4),
-        ('3', 3),
-        ('2', 2),
-    ]);
 
     sort_hand_group.iter_mut().for_each(|h| {
         h.sort_by(|a, b| {
@@ -103,29 +123,29 @@ mod day7_tests {
 
     #[test]
     fn day7_1_test() {
-        let hands = format_data(Path::new("src/day7/day7_input_test.txt"));
-        let result = total_winnings(hands);
+        let (hands,char_rank) = format_data(Path::new("src/day7/day7_input_test.txt"), false);
+        let result = total_winnings(hands, char_rank);
         assert_eq!(result, 6440);
     }
 
     #[test]
     fn day7_1_answer() {
-        let hands = format_data(Path::new("src/day7/day7_input.txt"));
-        let result = total_winnings(hands);
-        assert_eq!(result, 440000);
+        let (hands, char_rank) = format_data(Path::new("src/day7/day7_input.txt"), false);
+        let result = total_winnings(hands, char_rank);
+        assert_eq!(result, 246795406);
     }
 
     #[test]
     fn day7_2_test() {
-        let hands = format_data(Path::new("src/day7/day7_input_test.txt"));
-        let result = total_winnings(hands);
-        assert_eq!(result, 71503);
+        let (hands, char_rank) = format_data(Path::new("src/day7/day7_input_test.txt"), true);
+        let result = total_winnings(hands, char_rank);
+        assert_eq!(result, 5905);
     }
 
     #[test]
     fn day7_2_answer() {
-        let hands = format_data(Path::new("src/day7/day7_input.txt"));
-        let result = total_winnings(hands);
-        assert_eq!(result, 26187338);
+        let (hands, char_rank) = format_data(Path::new("src/day7/day7_input.txt"), true);
+        let result = total_winnings(hands, char_rank);
+        assert_eq!(result, 249356515);
     }
 }
