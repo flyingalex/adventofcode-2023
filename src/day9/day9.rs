7a39b3181b9c126b8537d5a3cb087448c619a943
Lin <hulin2021@gmail.com>
2023-12-10 00:36:20 +0100

    finish day9 second half

--- src/day9/day9.rs
+++ src/day9/day9.rs
@@ -31,7 +31,31 @@ fn get_extrapolated_values(histories: Vec<Vec<i64>>) -> i64 {
             last_nums.push(*new_created_history.last().unwrap());
             new_history = new_created_history;
         }
+        // rule: plus all last value
         total += &last_nums.iter().sum();
+    }
+    total
+}
+
+fn get_extrapolated_values2(histories: Vec<Vec<i64>>) -> i64 {
+    let mut total = 0;
+    for history in histories {
+        let mut first_nums = vec![*history.first().unwrap()];
+        let mut new_history = history.clone();
+        loop {
+            let mut new_created_history = vec![];
+            for window in new_history.windows(2).into_iter() {
+                new_created_history.push(window[1] - window[0]);
+            }
+            if new_history.iter().all(|&h| h == 0) {
+                break;
+            }
+            // inject to first, reverse it
+            first_nums.insert(0, *new_created_history.first().unwrap());
+            new_history = new_created_history;
+        }
+        // rule: (this is different) we need to subtract the last result
+        total += first_nums.into_iter().reduce(|acc, e| e - acc).unwrap();
     }
     total
 }
@@ -57,14 +81,14 @@ mod day9_tests {
     #[test]
     fn day9_2_test() {
         let histories = format_data(Path::new("src/day9/day9_input_test.txt"));
-        let result = get_extrapolated_values(histories);
-        assert_eq!(result, 6);
+        let result = get_extrapolated_values2(histories);
+        assert_eq!(result, 2);
     }
 
     #[test]
     fn day9_2_answer() {
         let histories = format_data(Path::new("src/day9/day9_input.txt"));
-        let result = get_extrapolated_values(histories);
-        assert_eq!(result, 13740108158591);
+        let result = get_extrapolated_values2(histories);
+        assert_eq!(result, -14929);
     }
 }
