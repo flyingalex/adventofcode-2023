d5d9aa1e03f6d4fbf738f48dbc9752a02af31c94
Lin <hulin2021@gmail.com>
2023-12-15 23:37:19 +0100

    finish day15 second half

--- src/day15/day15.rs
+++ src/day15/day15.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::path::Path;
+use std::process::id;
 use itertools::Itertools;
 use crate::utils::read_lines;
 
@@ -24,11 +25,61 @@ fn get_result_of_sequence(data: Vec<Vec<char>>) -> u64 {
     for datum in data {
         let mut start = 0;
         datum.iter().for_each(|&c| {
-            start = ((start + c as u32) * 17) % 256;
+            start = ((start + c as u64) * 17) % 256;
         });
-        result += start as u64;
+        result += start;
     }
     result
+}
+
+fn get_result_of_sequence2(data: Vec<Vec<char>>) -> u64 {
+    let mut map: HashMap<u64, Vec<(String, u64)>> = HashMap::new();
+    let mut len_pos: HashMap<String, u64> = HashMap::new();
+    for datum in data {
+        // HASH to find box idx
+        let mut new_idx = 0;
+        let mut label = String::new();
+        for (datum_idx, &c) in datum.iter().enumerate() {
+            if c == '-' {
+                // remove label in box if found
+                if let Some(idx) = len_pos.get(label.as_str()) {
+                    let mut found_pos_lens = map.get_mut(idx).unwrap();
+                    let index = found_pos_lens.iter().position(|(cur_label, _)| *cur_label == label).unwrap();
+                    found_pos_lens.remove(index);
+                    len_pos.remove(label.as_str());
+                }
+                break;
+            }
+
+            if c == '=' {
+                // update label length in box if found
+                let num = datum[datum_idx + 1].to_digit(10).unwrap() as u64;
+                if let Some(idx) = len_pos.get(label.as_str()) {
+                    let mut found_pos_lens = map.get_mut(idx).unwrap();
+                    let index = found_pos_lens.iter().position(|(cur_label, _)| *cur_label == label).unwrap();
+                    found_pos_lens[index].1 = num;
+                } else {
+                    // or add label in box if not found
+                    if let Some(len) = map.get_mut(&new_idx) {
+                        len.push((label.clone(), num));
+                    } else {
+                        map.insert(new_idx, vec![(label.clone(), num)]);
+                    }
+                    len_pos.insert(label, new_idx);
+                }
+                break;
+            }
+
+            new_idx = ((new_idx + c as u64) * 17) % 256;
+            label += c.to_string().as_str();
+        }
+    }
+
+    map.iter().map(|(box_idx, pos_lens)| {
+        pos_lens.iter().enumerate().map(|(slot_idx, (_, len))| {
+            (box_idx + 1) * (slot_idx + 1) as u64 * len
+        }).sum::<u64>()
+    }).sum()
 }
 
 #[cfg(test)]
@@ -52,14 +103,14 @@ mod day15_tests {
     #[test]
     fn day15_2_test() {
         let data = format_data(Path::new("src/day15/day15_input_test.txt"));
-        let result = get_result_of_sequence(data);
-        assert_eq!(result, 64);
+        let result = get_result_of_sequence2(data);
+        assert_eq!(result, 145);
     }
 
     #[test]
     fn day15_2_answer() {
         let data = format_data(Path::new("src/day15/day15_input.txt"));
-        let result = get_result_of_sequence(data);
+        let result = get_result_of_sequence2(data);
         assert_eq!(result, 90551);
     }
 }
