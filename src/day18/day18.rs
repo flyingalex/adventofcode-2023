3872ad2d1a572d28c542f1fb238092151d7e6c6f
Lin <hulin2021@gmail.com>
2023-12-19 19:45:53 +0100

    finish day18 second half

--- src/day18/day18.rs
+++ src/day18/day18.rs
@@ -1,12 +1,11 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::path::Path;
 use std::vec;
-use itertools::Itertools;
 use regex::Regex;
 use crate::utils::read_lines;
 
 
-fn format_data(file: &Path) -> Vec<(String, u32, String)> {
+fn format_data(file: &Path) -> Vec<(String, i64)> {
     let mut data = vec![];
     read_lines(file)
         .unwrap()
@@ -14,16 +13,40 @@ fn format_data(file: &Path) -> Vec<(String, u32, String)> {
             let strs = l.unwrap();
             let re = Regex::new(r"(?P<dir>[RLDU]) (?P<count>[0-9]+) \((?P<color>#[0-9a-z]+)\)").unwrap();
             let caps = re.captures(&strs).unwrap();
-            data.push((caps["dir"].to_string(),
-                       caps["count"].to_string().parse::<u32>().unwrap(),
-                       caps["color"].to_string(),
+            data.push((
+                caps["dir"].to_string(),
+                caps["count"].to_string().parse::<i64>().unwrap()
+            ));
+        });
+    data
+}
+
+fn format_data2(file: &Path) -> Vec<(String, i64)> {
+    let mut data = vec![];
+    read_lines(file)
+        .unwrap()
+        .for_each(|l| {
+            let strs = l.unwrap();
+            let re = Regex::new(r"\(#(?P<hex>[0-9a-z]{5})(?P<dir>[0123])\)").unwrap();
+            let caps = re.captures(&strs).unwrap();
+            let dir_num = caps["dir"].to_string().parse::<i64>().unwrap();
+            let dir = HashMap::from([
+                (0, "R"),
+                (1, "D"),
+                (2, "L"),
+                (3, "U"),
+            ]);
+
+            data.push((
+                dir.get(&dir_num).unwrap().to_string(),
+                i64::from_str_radix(caps["hex"].to_string().as_str(), 16).unwrap(),
             ));
         });
     data
 }
 
 // The Shoelace Algorithm
-fn shoelace_formula(v: Vec<(i32, i32)>) -> i32 {
+fn shoelace_formula(v: Vec<(i64, i64)>) -> i64 {
     let mut sum = 0;
     for pos in v.windows(2) {
         let first = pos[0];
@@ -34,22 +57,22 @@ fn shoelace_formula(v: Vec<(i32, i32)>) -> i32 {
     sum.abs() / 2
 }
 
-fn get_cubic_meters(data: Vec<(String, u32, String)>) -> i32 {
-    let mut directions: HashMap<&str, (i32, i32)> = HashMap::from([
+fn get_cubic_meters(data: Vec<(String, i64)>) -> i64 {
+    let directions: HashMap<&str, (i64, i64)> = HashMap::from([
         ("U", (-1, 0)),
         ("R", (0, 1)),
         ("D", (1, 0)),
         ("L", (0, -1)),
     ]);
-    let mut terrain: HashMap<(i32, i32), bool> = HashMap::new();
+    let mut terrain: HashMap<(i64, i64), bool> = HashMap::new();
     let mut current = (0, 0);
     terrain.insert(current, true);
     let mut vertices = vec![(0, 0)];
     let mut terrain_count = 1;
-    data.iter().for_each(|(dir, count, _)| {
+    data.iter().for_each(|(dir, count)| {
         let &(x, y) = directions.get(dir.as_str()).unwrap();
-        terrain_count += *count as i32;
-        current = (current.0 + x * *count as i32, current.1 + y * *count as i32);
+        terrain_count += *count;
+        current = (current.0 + x * *count, current.1 + y * *count);
         vertices.push(current);
     });
     vertices.push((0, 0));
@@ -78,15 +101,15 @@ mod day18_tests {
 
     #[test]
     fn day18_2_test() {
-        let data = format_data(Path::new("src/day18/day18_input_test.txt"));
+        let data = format_data2(Path::new("src/day18/day18_input_test.txt"));
         let result = get_cubic_meters(data);
-        assert_eq!(result, 94);
+        assert_eq!(result, 952408144115);
     }
 
     #[test]
     fn day18_2_answer() {
-        let data = format_data(Path::new("src/day18/day18_input.txt"));
+        let data = format_data2(Path::new("src/day18/day18_input.txt"));
         let result = get_cubic_meters(data);
-        assert_eq!(result, 1283);
+        assert_eq!(result, 90111113594927);
     }
 }
