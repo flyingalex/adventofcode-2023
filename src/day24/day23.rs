425abea63703fc4262b83e0be25efbeb33f461a1
Lin <hulin2021@gmail.com>
2023-12-24 22:20:08 +0100

    not finish day23 second part

--- src/day24/day23.rs
+++ src/day24/day23.rs
@@ -19,40 +19,6 @@ fn bread_first_search(mark: Vec<(usize, usize)> , map: &Vec<Vec<char>>, start: (
         result.insert(mark.len());
         return;
     }
-    let directions: HashMap<char, (i32, i32)> = HashMap::from([
-        ('^', (-1, 0)),
-        ('>', (0, 1)),
-        ('v', (1, 0)),
-        ('<', (0, -1)),
-    ]);
-    for i in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-        let next = (start.0 as i32 + i.0, start.1 as i32 + i.1);
-        if next.0 >= 0 && next.0 < map.len() as i32 && next.1 >= 0 && next.1 < map[0].len() as i32 {
-            let next = (next.0 as usize, next.1 as usize);
-            if !mark.contains(&next) {
-                if map[next.0][next.1] == '.' {
-                    let mut mark = mark.clone();
-                    mark.push(next);
-                    crate::day23::day23::bread_first_search(mark, map, (next.0, next.1), end, result);
-                }
-                if let Some(current) = directions.get(&map[next.0][next.1]) {
-                    let mut mark = mark.clone();
-                    mark.push(next);
-                    if !mark.contains(&((next.0 as i32 + current.0) as usize, (next.1 as i32 + current.1) as usize)) {
-                        mark.push(((next.0 as i32 + current.0) as usize, (next.1 as i32 + current.1) as usize));
-                        crate::day23::day23::bread_first_search(mark, map, ((next.0 as i32 + current.0) as usize, (next.1 as i32 + current.1) as usize), end, result);
-                    }
-                }
-            }
-        }
-    }
-}
-
-fn bread_first_search2(mark: Vec<(usize, usize)> , map: &Vec<Vec<char>>, start: (usize, usize), end: (usize, usize), result: &mut HashSet<usize>) {
-    if start == end {
-        result.insert(mark.len());
-        return;
-    }
 
     for i in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
         let next = (start.0 as i32 + i.0, start.1 as i32 + i.1);
@@ -75,17 +41,7 @@ fn get_longest_hikes(map: Vec<Vec<char>>) -> usize {
     let end = (map.len() - 1, end_y);
     let mark: Vec<(usize, usize)> = vec![];
     bread_first_search(mark, &map, start, end, &mut result);
-    result.iter().max().unwrap().to_owned()
-}
-
-fn get_longest_hikes2(map: Vec<Vec<char>>) -> usize {
-    let mut result = HashSet::new();
-    let start_y = map[0].iter().position(|c| *c == '.').unwrap();
-    let end_y = map[map.len() - 1].iter().position(|c| *c == '.').unwrap();
-    let start = (0, start_y);
-    let end = (map.len() - 1, end_y);
-    let mark: Vec<(usize, usize)> = vec![];
-    bread_first_search2(mark, &map, start, end, &mut result);
+    println!("{:?}", result);
     result.iter().max().unwrap().to_owned()
 }
 
@@ -110,14 +66,14 @@ mod day23_tests {
     #[test]
     fn day23_2_test() {
         let data = format_data(Path::new("src/day23/day23_input_test.txt"));
-        let result = get_longest_hikes2(data);
+        let result = get_longest_hikes(data);
         assert_eq!(result, 7);
     }
 
     #[test]
     fn day23_2_answer() {
         let data = format_data(Path::new("src/day23/day23_input.txt"));
-        let result = get_longest_hikes2(data);
+        let result = get_longest_hikes(data);
         assert_eq!(result, 128163929109524);
     }
 }
