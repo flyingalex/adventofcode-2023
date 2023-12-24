aa1fdfce38192b824e495e8e15bbd0d2bde3e163
Lin <hulin2021@gmail.com>
2023-12-24 18:26:52 +0100

    finish day22 second part

--- src/day22/day22.rs
+++ src/day22/day22.rs
@@ -69,6 +69,65 @@ fn get_bricks(bricks: &mut Vec<Vec<i32>>) -> u64 {
     total
 }
 
+// https://github.com/hyper-neutrino/advent-of-code/blob/main/2023/day22p2.py
+fn get_bricks2(bricks: &mut Vec<Vec<i32>>) -> u64 {
+    for brick_i in 0..bricks.len() {
+        let mut max_z = 1;
+        for check_i in 0..brick_i {
+            if overlap(&bricks[brick_i], &bricks[check_i]) {
+                max_z = max_z.max(bricks[check_i][5] + 1);
+            }
+        }
+        bricks[brick_i][5] -= bricks[brick_i][2] - max_z;
+        bricks[brick_i][2] = max_z;
+    }
+    bricks.sort_by(|a, d| a[2].cmp(&d[2]));
+
+    let mut k_supports_v = HashMap::new();
+    for i in 0..bricks.len() {
+        k_supports_v.insert(i, HashSet::new());
+    }
+    let mut v_supports_k = HashMap::new();
+    for i in 0..bricks.len() {
+        v_supports_k.insert(i, HashSet::new());
+    }
+
+    for (j, upper) in bricks.iter().enumerate() {
+        for (i, lower) in bricks[..j].iter().enumerate() {
+            if overlap(lower, upper) && upper[2] == lower[5] + 1 {
+                k_supports_v.get_mut(&i).unwrap().insert(j);
+                v_supports_k.get_mut(&j).unwrap().insert(i);
+            }
+        }
+    }
+
+    let mut total = 0;
+    for i in 0..bricks.len() {
+        let mut q = vec![];
+        let mut falling = HashSet::new();
+        for j in k_supports_v[&i].iter() {
+            if v_supports_k[j].len() == 1 {
+                q.push(*j);
+                falling.insert(*j);
+            }
+        }
+        falling.insert(i);
+        let mut tmp_k = HashSet::new();
+        while !q.is_empty() {
+            falling.extend(tmp_k.clone());
+            let j = q.remove(0);
+            for &k in k_supports_v[&j].difference(&falling) {
+                if v_supports_k[&k].is_subset(&falling) {
+                    q.push(k);
+                    tmp_k.insert(k);
+                }
+            }
+        }
+        total += falling.len() - 1
+    }
+    total as u64
+}
+
 #[cfg(test)]
 mod day22_tests {
     use super::*;
@@ -90,14 +149,14 @@ mod day22_tests {
     #[test]
     fn day22_2_test() {
         let mut data = format_data(Path::new("src/day22/day22_input_test.txt"));
-        let result = get_bricks(&mut data);
-        assert_eq!(result, 167004);
+        let result = get_bricks2(&mut data);
+        assert_eq!(result, 7);
     }
 
     #[test]
     fn day22_2_answer() {
         let mut data = format_data(Path::new("src/day22/day22_input.txt"));
-        let result = get_bricks(&mut data);
+        let result = get_bricks2(&mut data);
         assert_eq!(result, 128163929109524);
     }
 }
